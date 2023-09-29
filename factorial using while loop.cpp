206d748497badc24215ff61da7023499e62a9f4d
ShaileshPatil2000 <92880083+ShaileshPatil2000@users.noreply.github.com>
2023-09-28 23:20:12 -0700

    Add files via upload

---
+++ factorial using while loop.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n,i=1,fact=1;
+	cout<<"Enter number="<<endl;
+	cin>>n;
+	while(i<=n)
+	{
+
+		fact=fact*i;
+			i++;
+	
+	}
+		cout<<fact<<endl;
+
+}
